6a1711707292dd71e386da852cd4f40f1cdc05b9
christan <christan_price2008@yahoo.com>
2019-11-15 23:48:05 -0800

    added highs and lows to the forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,14 @@ const forecast = (lat, long, callback)=>{
     request({url, json: true}, (error, response)=>{
         const {code} = response.body;
         const {temperature, precipProbability} = response.body.currently;
+        const {temperatureHigh, temperatureLow} = response.body.daily.data[0]
         if (error){
             callback("Unable to connect to forecast services.", undefined);
         } else if (code === 400){
             callback("Unable to find location. Try another search!", undefined);
         }else{
             callback(undefined, {
-                temp : "It is currently " + temperature + " degrees out.",
+                temp : "It is currently " + temperature + " degrees out with a high of " + temperatureHigh + " and a low of " + temperatureLow + ".",
                 rain : "There is a " + precipProbability + "% chance of rain."
             })
         }
